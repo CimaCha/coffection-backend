23ccf5886c9d346af554083c46c4b1f3ff515ee1
maksim-cha <cimacha@yandex.ru>
2023-08-05 00:49:25 +0300

    add app

---
+++ src/test/java/com/sm/coffee/coffection/CoffectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.coffee.coffection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
